d0cf9564fa148aafc4aac35c49092facf2e0f02a
sadelcarpio <sadelcarpioa@gmail.com>
2021-10-23 14:34:57 -0500

    Se hizo el navbar

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,18 @@
-function myFunction(x) {
+function myFunction(x) { 
+    console.log(x);
+    x.classList.toggle("change");
+    var y = document.getElementById("myLinks");
+    if (y.style.display == "block") {
+      y.style.display = "none";
+    } else {
+      y.style.display = "block";
+      y.style.position = "fixed";
+    }
+  }
+
+  function dissapear(y) {
+    y.style.display = "none";
+    var x = document.getElementsByClassName("topnav")[0];
+    console.log(x);
     x.classList.toggle("change");
   }
