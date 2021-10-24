3d35b25c36bc519eb48514ea1aba64127da01330
sadelcarpio <sadelcarpioa@gmail.com>
2021-10-24 13:08:28 -0500

    aparecer

--- js/javascript.js
+++ js/javascript.js
@@ -9,8 +9,25 @@ function myFunction(x) {
     }
   }
 
-  function dissapear(y) {
-    y.style.display = "none";
-    var x = document.getElementsByClassName("topnav")[0];
-    x.classList.toggle("change");
-  }
+function dissapear(y) {
+  y.style.display = "none";
+  var x = document.getElementsByClassName("topnav")[0];
+  x.classList.toggle("change");
+}
+
+let lis = document.querySelectorAll('li');
+console.log(lis);
+
+// Descripción y precio de las imágenes
+
+li_test = lis[0];
+img = li_test.querySelector('img');
+p = li_test.querySelector('p');
+img.addEventListener('click', () => p.style.display = 'block');
+
+
+// for (li of lis) {
+//   img = li.querySelector('img');
+//   p = li.querySelector('p');
+//   console.log(p);
+// }
